6e432f24790220520953f9b49bef3155c6fd0aeb
Anatole-DC <adechauveron@gmail.com>
2021-10-19 11:06:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# TP3 COFFEE MACHINE
+
+**Language :** Java
+
+**Technos :** Spring Boot
+
+**Author :** Anatole  de Chauveron
+
+** Licence :** All rights reserved
+
+Spring boot coffee machine.
---
+++ src/test/java/cybertp3/coffeemachine/CoffeeMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package cybertp3.coffeemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
